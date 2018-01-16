fa4bd01eb3af7538b85ce2761043e6b24ec686e6
JessRho713 <gderobertis@ucvts.tec.nj.us>
2018-01-16 14:43:27 +0000

    comitted pictures

---
+++ FinalProject.py
@@ -0,0 +1,99 @@
+
+#The player is trapped in a dragon's lair, and they were caught trying to steal some of the dragon's hoarded gold. The dragon wakes up and considers eating the player, but instead decides to entertain itself by asking the player riddles.
+first_string ="Welcome. You are a petty thief, and you have learned that there is a chamber filled with gold and jewels inside the old castle on the hill.\n Seems too good to be true, but you decide to check it out.\nPress enter to continue."
+print(first_string)
+input()
+castle=("""
+
+            _   _   _                                               _   _   _
+           | |_| |_| |                                             | |_| |_| |
+           |         |                                             |         |
+           |         |                                             |         |
+           |         |                                             |         |
+           |         |       __      __     __     __     __       |         |
+           |          \_____|  |____|  |___|  |___|  |___|  |_____/          |
+           |                                                                 |
+           |                                                                 |
+           |                                                                 |
+           |                                                                 |
+           |                          _____________                          |
+           |                         /             \                         |
+           |                        /               \                        |
+           |                       /                 \                       |
+           |                      /                   \                      |
+           |                      |                   |                      |
+           |                      |                   |                      |
+           |                      |                   |                      |
+           |                      |                   |                      |
+           |______________________|___________________|______________________|""")
+print(castle)
+input()
+
+harp= ("""
+           _____________________________
+           | | | | | | | | | | | | | | ||
+           | | | | | | | | | | | | | | | |      /|
+           | | | | | | | | | | | | | | | ||    / ||
+           | | | | | | | | | | | | | | | | |  /| | |       _
+           | | | | | | | | | | | | | | | | ||/ | | ||      |
+           | | | | | | | | | | | | | | | | | | | | | |     |
+           | | | | | | | | | | | | | | | | | | | | | /   |-|
+           | | | | | | | | | | | | | | | | | | | | |/
+            || | | | | | | | | | | | | | | | | | | /   _
+             | | | | | | | | | | | | | | | | | | |/   |
+              || | | | | | | | | | | | | | | | | /  |-|
+               | | | | | | | | | | | | | | | | |/
+                || | | | | | | | | | | | | | | /
+                 | | | | | | | | | | | | | | |/
+                  || | | | | | | | | | | | | /
+                   | | | | | | | | | | | | |/
+                    ||_|_|_|_|_|_|_|_|_|_|_/""")
+
+print(harp)
+input()
+dragonmad= ("""||     ||
+               ||_____||
+               /       |   _____________          _
+              /|0| |0| |  /  ________  /      ___||
+             /         | /  /       / /      /____|
+            |          \/  /       / /_____/ /
+            |   v--v                    ____/
+             \______|                  /
+                    | _____________   /
+                     | |            | |
+                     |_|            |_|""")
+
+dragon= ("""||     ||
+            ||_____||
+            /       |   _____________          _
+           /|-| |-| |  /  ________  /      ___||
+          /         | /  /       / /      /____|
+         |          \/  /       / /_____/ /
+         |    o                      ____/
+          \______|                  /
+                 | _____________   /
+                 | |            | |
+                 |_|            |_|""")
+
+
+awakedragon=("""||    ||
+                 ||____||
+                /       |   _____________          _
+               /|0| |0| |  /  ________  /      ___||
+              /         | /  /       / /      /____|
+             |          \/  /       / /_____/ /
+             |    v                      ____/
+              \______|                  /
+                     | _____________   /
+                     | |            | |
+                     |_|            |_|""")
+
+
+
+print(dragon)
+print(dragonmad)
+print(awakedragon)
+
+
+
+print
