e1b2497ce16a48e408ca198d2b4fe356ec312676
JessRho713 <gderobertis@ucvts.tec.nj.us>
2018-01-20 22:34:49 +0000

    comitted

--- FinalProject.py
+++ FinalProject.py
@@ -1,8 +1,8 @@
 
 #The player is trapped in a dragon's lair, and they were caught trying to steal some of the dragon's hoarded gold. The dragon wakes up and considers eating the player, but instead decides to entertain itself by asking the player riddles.
-first_string ="Welcome. You are a petty thief, and you have learned that there is a chamber filled with gold and jewels inside the old castle on the hill.\n Seems too good to be true, but you decide to check it out.\nPress enter to continue."
-print(first_string)
-input()
+first_string ="Welcome. You are a petty thief, and you have learned that there is a chamber filled with gold and jewels inside the old castle on the hill.\n Seems too good to be true, but you decide to check it out.\nPress enter to continue whenever you see >>>."
+# I set up the '>>>' so when the player sees that symbol, they hit enter. This saves me from having to remind the player to hit enter.
+# I put all of the drawings in the same place so that I can reference them all at once.
 castle=("""
 
             _   _   _                                               _   _   _
@@ -26,8 +26,6 @@ castle=("""
            |                      |                   |                      |
            |                      |                   |                      |
            |______________________|___________________|______________________|""")
-print(castle)
-input()
 
 harp= ("""
            _____________________________
@@ -49,8 +47,7 @@ harp= ("""
                    | | | | | | | | | | | | |/
                     ||_|_|_|_|_|_|_|_|_|_|_/""")
 
-print(harp)
-input()
+
 dragonmad= ("""||     ||
                ||_____||
                /       |   _____________          _
@@ -63,7 +60,8 @@ dragonmad= ("""||     ||
                      | |            | |
                      |_|            |_|""")
 
-dragon= ("""||     ||
+dragon=("""
+            ||     ||
             ||_____||
             /       |   _____________          _
            /|-| |-| |  /  ________  /      ___||
@@ -73,10 +71,12 @@ dragon= ("""||     ||
           \______|                  /
                  | _____________   /
                  | |            | |
-                 |_|            |_|""")
+                 |_|            |_|
+                                    """)
 
 
-awakedragon=("""||    ||
+awakedragon=("""
+                 ||    ||
                  ||____||
                 /       |   _____________          _
                /|0| |0| |  /  ________  /      ___||
@@ -88,12 +88,54 @@ awakedragon=("""||    ||
                      | |            | |
                      |_|            |_|""")
 
+talkdragon=("""
+                 ||    ||
+                 ||____||
+                /       |   _____________          _
+               /|0| |0| |  /  ________  /      ___||
+              /         | /  /       / /      /____|
+             |          \/  /       / /_____/ /
+             |    o                      ____/
+              \______|                  /
+                     | _____________   /
+                     | |            | |
+                     |_|            |_|""")
+#I am going to put a list of riddles that the dragon can pick from to ask the player.
 
 
+#Defining the character's attributes
+class character():
+    def __init__(self):
+        self.hearts=3
+        self.correct=0
+        self.wrong=0
+        self.description_status=(f"\n<<<You currently have {self.hearts} more chances. You have gotten {self.correct} riddles right, and {self.wrong} wrong>>>")
+test= character()
+#Starting the game >>>>>>
+
+print(first_string)
+input()
+print(castle)
+print("You make it up to the old castle on the hill. Once inside, you find that the floors are covered in gold and jewels.\n However, one wrong move could land you in the dragon's path. \n >>> ")
+input()
 print(dragon)
-print(dragonmad)
+print("Oh, no! It's the dragon. However, it looks as if it is fast asleep. \n >>>")
+input()
+print("The sound of your boots echoes through the hallways. You enter the great throne room, where the dragon is sleeping on a heap of gold.\n On the other side of the room, you see a magic harp playing music. \n >>>.")
+input()
+print(harp)
+print("The dragon stirs in its sleep and swishes its tail. It knocks over the harp! \n The harp stops playing and clatters across the floor. \n >>>.")
+input()
 print(awakedragon)
+print("The dragon woke up!\n>>>")
+input()
+print(talkdragon)
+print("-Oi, wotcher little one!- \n Says the dragon in a cheerful aristocratic accent. \n -Best ya com\'up \'ere on sucha fine day! Ya look pretty tasty, mind if I take a wee bite?- >>>")
+print(awakedragon)
+print("The dragon considers for a bit and then changes its mind. -Oi, never tha mind that. Entertain me instead. I hope ya like riddles-")
+print(test.description_status)
 
 
 
-print
+
+
